c0de3bb2efbaf25499a1acbe911937efb0e4cee9
Marc <manjaroman2@protonmail.com>
2024-02-09 20:15:21 +0100

    patch

--- setup.sh
+++ setup.sh
@@ -1,28 +1,3 @@
-#!/bin/bash 
+#!/bin/bash
 
-pck_file=""
-
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "supply first arg ex dir" 
-    echo "supply second arg name" 
-    exit 1 
-fi 
-if [ ! -z "$3" ]; then
-    pck_file="$3"
-else
-    if [ -z "$pck_file" ]; then 
-        echo "provide pck file" 
-        exit 1 
-    fi 
-fi 
-
-cd .. 
-./decomp-3.5.3.sh $pck_file $1
-cd $1
-
-chmod +x $name/stuff.sh 
-$name/stuff.sh 
-
-# echo 
-echo "python $2/apply.py" 
-echo "python $2/create_patch.py" 
+# put your stuff here 
