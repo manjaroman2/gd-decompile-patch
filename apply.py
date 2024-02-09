c05c175d99645f29be2475ebb5aa3dfc2549c4e1
Marc <manjaroman2@protonmail.com>
2024-02-09 20:02:04 +0100

    init

---
+++ apply.py
@@ -0,0 +1,30 @@
+from pathlib import Path 
+from argparse import ArgumentParser
+import subprocess
+
+p = ArgumentParser()
+p.add_argument("--hash", type=str)
+args = p.parse_args()
+
+base = Path(__file__).parent
+
+def apply(file: Path):
+    # print(f"cd {base.parent} && git apply {file}")
+    print(file.read_text())
+    subprocess.check_output(f"cd {base.parent} && git apply {file}", shell=True)
+
+if args.hash:
+    for patch in base.glob("*.patch"):
+        hashed, timestamp, version = patch.with_suffix("").name.split("_")
+        if hashed == args.hash: 
+            apply(patch)
+            break
+else:
+    latest = 0
+    latest_patch = None 
+    for patch in base.glob("*.patch"):
+        hashed, timestamp, version = patch.with_suffix("").name.split("_")
+        if int(timestamp) > latest:
+            latest = int(timestamp)
+            latest_patch = patch 
+    apply(latest_patch)
---
+++ create_patch.py
@@ -0,0 +1,34 @@
+from pathlib import Path 
+from time import time 
+from hashlib import sha1
+import subprocess
+
+base = Path(__file__).parent
+version = ""
+for line in (base.parent / "gdre_export.log").read_text().splitlines():
+    if line.startswith("Version: "):
+        version = line.split("Version: ")[1]
+        break
+
+
+def run_cmd(cmd):
+    # print(cmd)
+    return subprocess.check_output(cmd, shell=True)
+
+if gitdiff := run_cmd(f"cd {base.parent} && git diff"):
+    patch_hashed = sha1(gitdiff).hexdigest()
+    for patch in base.glob("*.patch"):
+        hashed, timestamp, version = patch.with_suffix("").name.split("_")
+        if patch_hashed == hashed:
+            print(f"{patch} already exists, exiting...")
+            break
+    else:
+        timestamp = int(time())
+        patch_file = base/f"{patch_hashed}_{timestamp}_{version}.patch"
+        if patch_file.is_file():
+            exit(1)
+        print(f"writing patch {patch_file.name}")
+        patch_file.write_bytes(gitdiff)
+        print(run_cmd(f"cd {base} && git add . && git commit -am \"{timestamp}\" && git push").decode())
+else:
+    print("No changes detected!")
